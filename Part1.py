ec969718f1107e64e706c7018a39e5de041bd591
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-04-27 21:50:17 +0700

    Update Part1.py
    Remove un-needed line

--- Part1.py
+++ Part1.py
@@ -4,7 +4,6 @@ def ask_and_sum():
     """
     num1 = int(input("Adad aval ra vared konid: "))
     num2 = int(input("Adad dovom ra vared konid: "))
-    sum = num1 + num2
-    return(f"majmo adad {num1} va {num2} barabar ba {sum} mibashad.")
+    return(f"majmo adad {num1} va {num2} barabar ba {num1+num2} mibashad.")
 
 print(ask_and_sum())
