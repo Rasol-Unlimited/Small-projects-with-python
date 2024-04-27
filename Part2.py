342cc05a8c3d5b356fcffdb4be7e07c0cae27aaf
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-04-27 21:57:53 +0700

    Update Part2.py
    Implemented for loop instead of while on line 12 as per standard
    changed nested function calls into non-recursive functions

--- Part2.py
+++ Part2.py
@@ -1,15 +1,17 @@
 def ask_count_num():
-    numbercount = int(input("che tedad adad ra mikhahid ba ham jam konid? "))
-    if numbercount <= 0:
-        print("adad balatar az 0 vared konid.")
-        ask_count_num()
+    while True:
+        try:
+            numbercount = int(input("che tedad adad ra mikhahid ba ham jam konid? "))
+            if numbercount <= 0:
+                raise AssertionError
+            break
+        except AssertionError:
+            print("adad balatar az 0 vared konid.")
         return
-    count = 0
     total_sum = 0
-    while count != numbercount :
+    for i in range(numbercount):
         number = int(input(f"adad {count+1} ra vared konid: "))
         total_sum += number
-        count += 1
     
     print(f"majmo adad barabar ba {total_sum} mibashad.")
 
