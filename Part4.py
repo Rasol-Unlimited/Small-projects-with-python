29cda07ebcd6e31dedfee743ecaa087dd0bd4e2a
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-05-18 04:31:32 +1000

    Update Part4.py (#2)
    Removed highly unnerseary functions

--- Part4.py
+++ Part4.py
@@ -1,12 +1,3 @@
-x = int(input('x: '))
-low = 0
-high = x + 1
+import math
+print(math.sqrt(int(input('x: ')))
 
-while low < high:
-    mid = (high + low) // 2
-    if mid * mid <= x:
-        low = mid + 1
-    else:
-        high = mid
-
-print(low - 1)
