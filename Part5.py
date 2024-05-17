b7091d818de457ec90bfc05f02d429c38eecb915
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-05-18 04:31:04 +1000

    Update Part5.py (#3)
    Cleaned up code

--- Part5.py
+++ Part5.py
@@ -1,27 +1,11 @@
 def is_prime(x):
 
-  if x <= 1:
-    return False
-  elif x <= 3:
-    return True
-  elif x % 2 == 0 or x % 3 == 0:
-    return False
-
-  i = 5
-  while i * i <= x:
-    if x % i == 0 or x % (i + 2) == 0:
+  for i in range(2,x):
+    if x % i == 0:
       return False
-    i += 6
-
-  return True
-
-x = int(input("yek adad vared konid (x >= 2): "))
+   return True  
 
 if is_prime(x):
   print(f"{x} adad aval mibashad")
 else:
-  
-  for i in range(2, x):
-    if x % i == 0:
-      print(f"{x} adad aval nmibashad. maqsoam alayih: {i}")
-      break
+  print(f"{x} adad aval nmibashad. maqsoam alayih: {i}")
